958aa7720a54ac4491fc70f90eb067bb3f52267c
Quang Trần Văn <quangtrn8821@gmail.com>
2023-10-01 23:32:47 +0700

    update layout

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,11 @@
+<style lang="scss" scoped>
+.layout-sidebar {
+  height: calc(100vh - 54px);
+  width: 260px;
+  box-shadow: 0 4px 40px rgb(39 32 120 / 10%);
+}
+</style>
+
+<template>
+  <div class="layout-sidebar flex items-center"></div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="layout-default w-full h-full">
     <LayoutHeader />
-    <nuxt-page></nuxt-page>
+
+    <div class="flex w-full">
+      <LayoutSidebar />
+
+      <nuxt-page class="page"></nuxt-page>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 .layout-default {
+  .page {
+    height: calc(100vh - 54px);
+    width: calc(100vw - 260px);
+    padding: 0px 16px;
+  }
 }
 </style>
