34f482e678ace269dd8c58cca5ed3fe454dc9a95
quangtv <quangtv@rabiloo.com>
2023-10-06 11:05:05 +0700

    update

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,12 +1,11 @@
 <style lang="scss" scoped>
 .layout-header {
-  height: 54px;
-  box-shadow: 0 4px 40px rgb(39 32 120 / 10%);
+  height: 64px;
 }
 </style>
 
 <template>
-  <div class="layout-header flex items-center">
+  <div class="layout-header flex items-center border-b border-gray-200">
     <GlobalLogo />
   </div>
 </template>
--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -2,10 +2,9 @@
 .layout-sidebar {
   height: calc(100vh - 54px);
   width: 260px;
-  box-shadow: 0 4px 40px rgb(39 32 120 / 10%);
 }
 </style>
 
 <template>
-  <div class="layout-sidebar flex items-center"></div>
+  <div class="layout-sidebar flex items-center border-r border-grey"></div>
 </template>
